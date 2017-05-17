f41ce7718d202c139f2471f2288727cc99c50af7
Conor Curry <conorcurry@gmail.com>
2017-05-16 20:40:36 -0500

    Use a django shortcut for 404s on detail views

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Question
@@ -18,11 +18,16 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+#def detail(request, question_id):
+#    try:
+#        quesion = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404 ("Question does not exist.")
+#    return render(request, 'polls/detail.html', {'question':question})
+
+#using get_object_or_404 as a shortcut for the above.
 def detail(request, question_id):
-    try:
-        quesion = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404 ("Question does not exist.")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
